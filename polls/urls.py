c92f6c37ebec04cb14c38d1a54876039ee7c4c11
nh7881 <nh7881@naver.com>
2020-02-16 13:46:20 +0900

    add templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name = 'index'),
+    # ex : /polls/5
+    path('<int:question_id>/', views.detail, name = 'detail'),
+    # ex : /polls/5/results/
+    path('<str:question_id>/results/', views.results, name = 'result'),
+    # ex : /polls/5/votes/
+    path('<int:question_id>/votes/', views.votes, name = 'votes'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse as HR
+from .models import Question
+from django.template import loader
 
 def index(request) :
-    return HttpResponse('hi heon')
+    question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'question_list' : question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+from django.shortcuts import get_object_or_404
+
+def detail(request, question_id) :
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
+
+def results(request, question_id) :
+    response = "You're looking at the results of question {}".format(question_id)
+    return HR(response)
+
+def votes(request, question_id) :
+    var_type = type(question_id)
+    return HR("You're voting on question {}, type : {}".format(question_id, var_type))
